1f4795397f0c54d7c52170ab34778d9a9b8ab776
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-02-02 14:43:55 -0600

    add documentation to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,93 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+/* When a user clicks on a button, an element with the `.modal` class is opened. */
+document.addEventListener("DOMContentLoaded", () => {
   // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
-
     // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
     });
   }
 
   // Functions to open and close a modal
   function openModal($el) {
-    $el.classList.add('is-active');
+    $el.classList.add("is-active");
   }
-
+  
   function closeModal($el) {
-    $el.classList.remove('is-active');
+    $el.classList.remove("is-active");
   }
 
   function closeAllModals() {
-    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+    (document.querySelectorAll(".modal") || []).forEach(($modal) => {
       closeModal($modal);
     });
   }
 
   // Add a click event on buttons to open a specific modal
-  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+  (document.querySelectorAll(".js-modal-trigger") || []).forEach(($trigger) => {
     const modal = $trigger.dataset.target;
     const $target = document.getElementById(modal);
     console.log($target);
 
-    $trigger.addEventListener('click', () => {
+    $trigger.addEventListener("click", () => {
       openModal($target);
     });
   });
 
   // Add a click event on various child elements to close the parent modal
-  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot button') || []).forEach(($close) => {
-    const $target = $close.closest('.modal');
+  (
+    document.querySelectorAll(
+      ".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot button"
+    ) || []
+  ).forEach(($close) => {
+    const $target = $close.closest(".modal");
 
-    $close.addEventListener('click', () => {
+    $close.addEventListener("click", () => {
       closeModal($target);
     });
   });
 
   // Add a keyboard event to close all modals
-  document.addEventListener('keydown', (event) => {
+  document.addEventListener("keydown", (event) => {
     const e = event || window.event;
 
-    if (e.keyCode === 27) { // Escape key
+    if (e.keyCode === 27) {
+      // Escape key
       closeAllModals();
     }
   });
 });
 
+/**
+ * It removes the is-active class from the modal and adds the is-hidden class to the landing message.
+ * @param event - The event object that was triggered.
+ */
 function displayFelt(event) {
   event.preventDefault();
-  modal.classList.remove('is-active');
+  modal.classList.remove("is-active");
   console.log(event.target);
   // Prevents cancel from returning felt view
-    heroEl.style.backgroundImage = "url(./assets/images/red-felt.jpeg)";
-    landingMsg.classList.add('is-hidden');
-  };
-
+  heroEl.style.backgroundImage = "url(./assets/images/red-felt.jpeg)";
+  landingMsg.classList.add("is-hidden");
+}
 
 const formEl = document.getElementById("form-el");
-const modal = document.querySelector('.modal');
-const heroEl = document.querySelector('.hero');
-const landingMsg = document.getElementById('landing-msg');
+const modal = document.querySelector(".modal");
+const heroEl = document.querySelector(".hero");
+const landingMsg = document.getElementById("landing-msg");
 
 formEl.addEventListener("submit", displayFelt);
