2b10ad25f262b6ea31d37fe58ade7266b35fe73a
Andy MCA <AndrewMcAlpin@gmail.com>
2022-02-07 18:12:05 -0600

    switch func to async

--- assets/js/script.js
+++ assets/js/script.js
@@ -689,28 +689,53 @@ function endPlayerTurn() {
   playerCard3.removeEventListener("click", playCard);
   playerCard4.removeEventListener("click", playCard);
   // ENEMY INSULT MESSAGES GO HERE
-
-
-function fuckOff() {
-  // These variables are for the insult array
   let from = enemy.name;
-  let name = player.name;
-  
-    const randomIndex = Math.floor(Math.random() * insult.length)
+  const randomIndex = Math.floor(Math.random() * insult.length)
+  const apiUrlTwo = "https://foaas.com/" + insult[randomIndex] + "/" + from;
+async function fuckOff(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
-    const apiUrlTwo = "https://foaas.com/" + insult[randomIndex] + "/" + from;
+fuckOff(apiUrlTwo, {})
+  .then(data => {
+    console.log(data); // JSON data parsed by `data.json()` call
+  });
+
+
+// function fuckOff() {
+//   // These variables are for the insult array
+//   let from = enemy.name;
+//   let name = player.name;
+  
+//     const randomIndex = Math.floor(Math.random() * insult.length)
+
+//     const apiUrlTwo = "https://foaas.com/" + insult[randomIndex] + "/" + from;
         
     
-    fetch(apiUrlTwo)
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          console.log(data);
-        })
-      }
-      else (console.log("error"));
-    })
-  }
+  //   fetch(apiUrlTwo)
+  //   .then(function (response) {
+  //     if (response.ok) {
+  //       response.json().then(function (data) {
+  //         console.log(data);
+  //       })
+  //     }
+  //     else (console.log("error"));
+  //   })
+  // }
     // const randomI =
     // Use math random to pick the insult
     // insult = above
